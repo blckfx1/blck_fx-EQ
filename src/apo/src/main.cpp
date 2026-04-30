9814958a841e2c473e27e3e9ace32728379a4468
blckfx1 <zaripoz2002@gmail.com>
2026-05-01 03:47:49 +0500

    Fixed APO CMake and main.cpp for compilation

--- src/apo/src/main.cpp
+++ src/apo/src/main.cpp
@@ -1,5 +1,5 @@
 #include <windows.h>
-#include <audioenginebaseapo.h>
+#include <AudioEngineBaseAPO.h>
 
 static const GUID CLSID_BlackfoxCustomAPO = 
 { 0xa1b2c3d4, 0xe5f6, 0x4789, { 0xab, 0xcd, 0xef, 0x12, 0x34, 0x56, 0x78, 0x90 } };
